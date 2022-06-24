8626d731e1915a4976c543ddd88ae137bb1938ec
lumir <lumir@LAPTOP-7FFFN4H1>
2022-06-24 07:26:14 +0200

    Přidejte soubory projektu.

---
+++ Kontrola Emailu/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Napište email: ");
+string email = Console.ReadLine();
+string trim = email.Trim();
+if (email.Contains("@") && email.EndsWith(".cz"))
+{
+    Console.WriteLine($"Váš email je dlouhý {trim.Length} znaků");
+}
+else if (email.Contains("@") && email.EndsWith(".sk"))
+{
+    Console.WriteLine($"Váš email je dlhý {trim.Length} znaků.");
+}
+else if (email.Contains("@") && email.EndsWith(".com"))
+{
+    Console.WriteLine($"Your email is {trim.Length} characters long. ");
+}
+else
+{
+    Console.WriteLine("Toto není platný email");
+}
